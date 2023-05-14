2e1b920da04ea82592b4c5068e0bfe38f20d3151
GrauKatze <GrauKatze@protonmail.com>
2023-05-14 20:48:25 +0300

    init

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn plus(one: i32, two: i32) -> i32{
+    one + two
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn plus_work(){
+        let result = plus(23, 43);
+        assert_eq!(result,23+43);
+    }
+}
